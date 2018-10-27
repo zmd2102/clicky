071b5322ed13f3d3df36ecf82b2df977ded0e2fb
zmd2102 <zaiddoski@Zaids-MBP.fios-router.home>
2018-10-27 19:51:12 -0400

    Updates

---
+++ precache-manifest.6c70a07e91eb4758eec52e9b7aeaa179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e53dddbe16ca2509119",
+    "url": "/clicky/static/js/runtime~main.9e53dddb.js"
+  },
+  {
+    "revision": "02083d279b6d008df402",
+    "url": "/clicky/static/js/main.02083d27.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/clicky/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "02083d279b6d008df402",
+    "url": "/clicky/static/css/main.0a991b9f.chunk.css"
+  },
+  {
+    "revision": "fbd10f1a49a3db4ebc7de1b75965f536",
+    "url": "/clicky/index.html"
+  }
+];
